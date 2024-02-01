77a9dc8422335dd9fd59d8efa3fa471a371a9b57
JakeBuroker <shamrecks@gmail.com>
2024-02-01 11:35:03 -0600

    get route set

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -5,15 +5,27 @@ const router = express.Router();
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
-});
+  router.get('/', (req, res) => {
+    const query = `
+      SELECT * FROM "item"
+        ORDER BY "description" ASC;
+    `;
+    pool.query(query)
+      .then(result => {
+        res.send(result.rows);
+      })
+      .catch(err => {
+        console.log('ERROR: Get all movies', err);
+        res.sendStatus(500)
+      })
+  
+  });
 
 /**
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
-  // endpoint functionality
+ 
 });
 
 /**
